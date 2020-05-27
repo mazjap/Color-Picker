7f3a1222314d8a701bce866a4573d986891e51ac
Jordan Christensen <jordan.c4922@gmail.com>
2020-05-26 21:06:20 -0600

    Add initial xcode project and .gitignore

---
+++ Color Picker/Color Picker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Picker
+//
+//  Created by Jordan Christensen on 5/26/20.
+//  Copyright © 2020 Mazjap Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
