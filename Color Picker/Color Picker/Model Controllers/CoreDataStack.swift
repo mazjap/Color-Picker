4763978ad6ed39bef176b374a4f0dd5b29acfb6e
Jordan Christensen <jordan.c4922@gmail.com>
2020-05-26 23:54:33 -0600

    Add CoreData models and convenience initializers

---
+++ Color Picker/Color Picker/Model Controllers/CoreDataStack.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataStack.swift
+//  Color Picker
+//
+//  Created by Jordan Christensen on 5/26/20.
+//  Copyright © 2020 Mazjap Co. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    private init() {}
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "Color")
+        container.loadPersistentStores(completionHandler: { (_, error) in
+            if let error = error {
+                fatalError("Failed to load persistant stores: \(error)")
+            }
+        })
+        
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func saveToPersistentStore() {
+        do {
+            try mainContext.save()
+        } catch {
+            NSLog("Error saving context: \(error)")
+            mainContext.reset()
+        }
+    }
+}
